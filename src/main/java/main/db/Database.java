a2779cb70c408c1d571c1df97dfeade143c5e90e
Vedran Abramović <vabramovic@sciusa.com>
2017-01-11 18:04:59 +0100

    Created a Database class for creating and closing a connection.

--- src/main/java/main/db/Database.java
+++ src/main/java/main/db/Database.java
@@ -1,10 +1,48 @@
 package main.db;
 
-/**
- * Created by brama on 1/8/17.
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class Database {
+    private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
 
     public Database() {
+        try {
+            // This will load the MySQL driver, each DB has its own driver
+            Class.forName("com.mysql.jdbc.Driver");
+            // Setup the connection with the DB
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://sql7.freemysqlhosting.net/sql7152891?"
+                            + "user=sql7152891&password=2lfGA3yCas");
+        } catch (Exception e) {
+            System.out.print(e);
+        } finally {
+            close();
+        }
     }
+
+    private void close() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connect != null) {
+                connect.close();
+            }
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+    }
+
 }
