635e8b66098c294bf4bed49c4e7bd14fc4d972bc
Vedran Abramović <vabramovic@sciusa.com>
2017-01-11 19:36:00 +0100

    validateUser function is implemented and it returns 1 if user with the given username and password is found

--- src/main/java/main/db/UserAuthentication.java
+++ src/main/java/main/db/UserAuthentication.java
@@ -1,6 +1,6 @@
 package main.db;
 
-import main.models.AuthenticationResponse;
+import main.services.HashService;
 
 import java.sql.ResultSet;
 
@@ -8,9 +8,27 @@ import java.sql.ResultSet;
  * Created by brama on 1/11/17.
  */
 public class UserAuthentication extends Database {
-    public ResultSet validateUser(String username, String password) {
+    public int validateUser(String username, String password) {
+        String hashedPassword = new HashService().stringToMD5(password);
+        try {
+            String sql = "SELECT * FROM Users WHERE Users.name = ? AND Users.pass_hash = ?";
+            this.preparedStatement = this.connect.prepareStatement(sql);
+            this.preparedStatement.setString(1, username);
+            this.preparedStatement.setString(2, hashedPassword);
+            this.resultSet = this.preparedStatement.executeQuery();
 
-        return null;
+            int userCount = 0;
+            while (this.resultSet.next()) {
+                userCount++;
+                System.out.println("Username: " + this.resultSet.getString("name"));
+                System.out.println("Password: " + this.resultSet.getString("pass_hash"));
+            }
+            return userCount;
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        return 0;
     }
 
     public ResultSet logoutUser(String token) {
@@ -30,5 +48,6 @@ public class UserAuthentication extends Database {
     public void destroyToken(String token) {
 
     }
+
 
 }
