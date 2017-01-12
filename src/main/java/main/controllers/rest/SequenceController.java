f574e54329aad80f234917f9fc1a12c41039745e
Vedran Abramović <vabramovic@sciusa.com>
2017-01-12 15:14:23 +0100

    Implemented sequence creation

--- src/main/java/main/controllers/rest/SequenceController.java
+++ src/main/java/main/controllers/rest/SequenceController.java
@@ -1,14 +1,18 @@
 package main.controllers.rest;
 
+import main.db.SequenceListManager;
 import main.db.SequenceManager;
+import main.db.UserAuthentication;
 import main.models.Sequence;
 import main.models.SequenceList;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
-@RequestMapping("/rest")
 public class SequenceController {
 
     @RequestMapping("/sequence")
@@ -30,9 +34,9 @@ public class SequenceController {
          * Todo: Validate token first
          */
 
-        SequenceManager sequenceManager = new SequenceManager();
-        SequenceList sequenceList = sequenceManager.getSequenceList();
-        sequenceManager.close();
+        SequenceListManager sequenceListManager = new SequenceListManager();
+        SequenceList sequenceList = sequenceListManager.getSequenceList();
+        sequenceListManager.close();
 
         return sequenceList;
     }
@@ -46,10 +50,43 @@ public class SequenceController {
          * Todo: Validate token first
          */
 
-        SequenceManager sequenceManager = new SequenceManager();
-        SequenceList sequenceList = sequenceManager.getSequencePage(page, itemsPerpage, filter);
-        sequenceManager.close();
+        SequenceListManager sequenceListManager = new SequenceListManager();
+        SequenceList sequenceList = sequenceListManager.getSequenceListPage(page, itemsPerpage, filter);
+        sequenceListManager.close();
 
         return sequenceList;
     }
+
+    @RequestMapping(value = "/sequence/new", method = RequestMethod.POST)
+    public Sequence createSequence(@RequestParam(value = "token", defaultValue = "") String token,
+                                   @RequestParam(value = "sequenceNumber", defaultValue = "") Long sequenceNumber,
+                                   @RequestParam(value = "by_user", defaultValue = "") String by_user,
+                                   @RequestParam(value = "purpose", defaultValue = "") String purpose,
+                                   @RequestParam(value = "date", defaultValue = "") Date date) {
+        /**
+         * Todo: Validate token first
+         */
+        UserAuthentication userAuthentication = new UserAuthentication();
+        if (userAuthentication.getUserByToken(token) == "") {
+            return null;
+        }
+
+        SequenceManager sequenceManager = new SequenceManager();
+        Long nextAvailableSequenceNumber = sequenceManager.getNextAvailableSequenceNumber();
+
+        //Check if requested sequence number is not taken in the meantime.
+        if (nextAvailableSequenceNumber > sequenceNumber) {
+            //if it's taken, return a sequence with a new sequence number
+            return new Sequence(nextAvailableSequenceNumber, by_user, purpose, date);
+        } else {
+            //else, save the requested sequence
+            Sequence sequence;
+            sequence = new Sequence(sequenceNumber, by_user, purpose, date);
+            sequenceManager.createSequence(sequence);
+            sequenceManager.close();
+            return sequence;
+        }
+
+    }
+
 }
