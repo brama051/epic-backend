0c13381734ee0d5448e5e8c433d53c3b818c2b1f
Vedran Abramović <vabramovic@sciusa.com>
2017-01-12 10:00:31 +0100

    Removed tryout code

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,16 +1,17 @@
 package main;
 
+import main.db.MySQLAccess;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        /**
-         * Todo: Set up database on servlet startup
-         **/
         SpringApplication.run(Application.class, args);
+
     }
 
 }
