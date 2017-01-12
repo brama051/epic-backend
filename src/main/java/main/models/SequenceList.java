07877da245ec9fd875072fb0aae70337bab88c26
Vedran Abramović <vabramovic@sciusa.com>
2017-01-12 10:14:50 +0100

    Implemented SequenceList constructors and some other functions

--- src/main/java/main/models/SequenceList.java
+++ src/main/java/main/models/SequenceList.java
@@ -7,15 +7,36 @@ import java.util.List;
  * Created by brama on 1/12/17.
  */
 public class SequenceList {
-    public List<Sequence> list = new ArrayList<>();
-    public int listLength = 0;
+    public List<Sequence> list;
+    public int listLength;
 
+    public SequenceList() {
+        this.list = new ArrayList<>();
+        this.listLength = 0;
+    }
+
+    public SequenceList(Sequence sequence) {
+        this.list = new ArrayList<>();
+        this.list.add(sequence);
+        this.listLength = 1;
+    }
 
     public int getListLength() {
         return this.listLength;
     }
 
-    public void setListLength(int listLength) {
+    public void setListLength() {
         this.listLength = this.list.size();
     }
+
+    public void addSequence(Sequence sequence) {
+        this.list.add(sequence);
+        this.setListLength();
+    }
+
+    public List<Sequence> getList() {
+        return this.list;
+    }
+
+
 }
