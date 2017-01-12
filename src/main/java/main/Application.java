dcc6870830eb49333f71766fd6b0834683ba56f8
Vedran Abramovic <vedran.abramovic@gmail.com>
2017-01-12 21:49:51 +0100

    Removed unused test class

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,6 +1,6 @@
 package main;
 
-import main.db.MySQLAccess;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
