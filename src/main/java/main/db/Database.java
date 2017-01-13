f945efc7b5a79d600de6d95ee2fc1a21eea83846
Vedran Abramović <vabramovic@sciusa.com>
2017-01-13 20:34:04 +0100

    Fixed a typo in the new connection URL

--- src/main/java/main/db/Database.java
+++ src/main/java/main/db/Database.java
@@ -21,7 +21,7 @@ public class Database {
                     .getConnection("jdbc:mysql://" + System.getenv("MYSQL_URL") + "/" + System.getenv("MYSQL_DATABASE")
                             + "user=" + System.getenv("MYSQL_USER") + "&password=" + System.getenv("MYSQL_PASSWORD"));*/
             this.connect = DriverManager
-                    .getConnection("jdbc:mysql://" + System.getenv("MYSQL_URL") + "/" + System.getenv("MYSQL_DATABASE") + "?user=" + System.getenv("MYSQL_USER") + "&password=" + System.getenv("MYSQL_PASSWORD"));
+                    .getConnection("jdbc:mysql://" + System.getenv("MYSQL_URL") + "/" + System.getenv("MYSQL_DATABASE"), System.getenv("MYSQL_USER"), System.getenv("MYSQL_PASSWORD"));
         } catch (Exception e) {
             System.out.println(e.toString());
         }
