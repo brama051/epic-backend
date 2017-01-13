ed99e34665433befaf41f84e7b7e6d30961bfa18
Vedran Abramović <vabramovic@sciusa.com>
2017-01-13 10:17:35 +0100

    Added CORS

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -2,9 +2,7 @@ package main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
