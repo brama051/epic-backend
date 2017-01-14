7504cd3199511a839802769d5f5c9612d898c13e
Vedran Abramović <vabramovic@sciusa.com>
2017-01-14 19:58:43 +0100

    Debugging save method

--- src/main/java/main/db/SequenceManager.java
+++ src/main/java/main/db/SequenceManager.java
@@ -46,12 +46,12 @@ public class SequenceManager extends Database {
         }
 
         try {
-            String sql = "INSERT INTO sequences VALUES ( ?, ?, ?, ?)";
+            String sql = "INSERT INTO sequences VALUES ( ?, ?, ?)";
             this.preparedStatement = this.connect.prepareStatement(sql);
             this.preparedStatement.setLong(1, sequence.getSequenceNumber());
             this.preparedStatement.setString(2, sequence.getByUser());
             this.preparedStatement.setString(3, sequence.getPurpose());
-            this.preparedStatement.setDate(4, new java.sql.Date(sequence.getDate().getTime()));
+            //this.preparedStatement.setDate(4, new java.sql.Date(sequence.getDate().getTime()));
 
             this.preparedStatement.executeUpdate();
             return sequence;
